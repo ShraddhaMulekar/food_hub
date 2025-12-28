eed13ea73434f3ccba374d5b292004cd8b14e1f1
ShraddhaMulekar <mulekarshraddha@gmail.com>
2025-12-28 20:02:33 +0530

    feat: add MongoDB connection functionality

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
+const connectDB = require('./db')
 dotenv.config()
 
 const app = express()
@@ -11,4 +12,5 @@ app.use(cors())
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
+  connectDB()
 })
